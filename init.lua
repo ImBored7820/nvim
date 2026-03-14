4f8858fa775048a869a87ea0ae9d32b51f4f5098
musa <mohammedmusaali549@gmail.com>
2026-03-14 16:14:55 -0700

    Neovide Enhancements

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
+if vim.g.neovide then
+    vim.g.neovide_title_background_color = "#24273a" -- Macchiato Base
+    vim.g.neovide_title_text_color = "#cad3f5"       -- Macchiato Text
+    
+    vim.g.neovide_floating_shadow = true
+    vim.g.neovide_floating_z_height = 10
+    vim.g.neovide_light_radius = 5
+    vim.g.neovide_window_blurred = true
+
+end
+
+
 -- Bootstrap lazy.nvim before any other setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
